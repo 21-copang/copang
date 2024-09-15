8f5ef777b1f89a099ad3f8c2c812427c32d84c62
gichani <thdrlcks97@gmail.com>
2024-09-15 17:31:37 +0900

    [Add #17] CommonResponse 설정

---
+++ client/hub/src/main/java/com/sparta/copang/hub/presentation/response/status/CommonStatusCode.java
@@ -0,0 +1,16 @@
+package com.sparta.copang.hub.presentation.response.status;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum CommonStatusCode implements StatusCode {
+    OK(200, 200, "Success"),
+    BAD_REQUEST(400, 400, "Bad Request"),
+    SERVER_ERROR(500, 500, "Server Error");
+
+    private final Integer httpStatusCode;
+    private final Integer statusCode;
+    private final String description;
+}
